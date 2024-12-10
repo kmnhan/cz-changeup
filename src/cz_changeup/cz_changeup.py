3daa3906d675d19a7f0e1f269735a15157d2068c
Kimoon Han <khan@kaist.ac.kr>
2024-12-10 14:55:28 +0900

    feat: add support for linking external GitHub issues in changelog

--- src/cz_changeup/cz_changeup.py
+++ src/cz_changeup/cz_changeup.py
@@ -6,7 +6,7 @@ from commitizen import config, git
 from commitizen.cz.conventional_commits import ConventionalCommitsCz
 from jinja2 import PackageLoader
 
-_issue_keywords = (
+_ISSUE_KEYWORDS = (
     "close",
     "closes",
     "closed",
@@ -17,12 +17,30 @@ _issue_keywords = (
     "resolves",
     "resolved",
 )
-_issue_pattern = re.compile(
-    r"(\b(?:" + "|".join(_issue_keywords) + r")\s+(#\d+(\s+#\d+)*)\b)", re.IGNORECASE
+_ISSUE_PATTERN = re.compile(
+    r"(\b(?:" + "|".join(_ISSUE_KEYWORDS) + r")\s+(#\d+(\s+#\d+)*)\b)", re.IGNORECASE
+)
+_ISSUE_PATTERN_EXTERNAL = re.compile(
+    r"(?P<owner>[\w-]+)/(?P<repo>[\w-]+)/#(?P<issue_number>\d+)"
 )
 
 
 def linkify_issues(message: str, base_url: str) -> str:
+    """
+    Converts issue references after keywords in a message to markdown links.
+
+    Args:
+        message (str): The message containing issue references.
+        base_url (str): The base URL for the issue tracker.
+
+    Returns:
+        str: The message with issue references converted to markdown links.
+
+    Example:
+        >>> linkify_issues("This fixes #123.", "https://github.com/user/repo")
+        'This fixes [#123](https://github.com/user/repo/issues/123).'
+    """
+
     def _replace_issue_with_link(match):
         keyword = match.group(1)
         issue_numbers = match.group(2).split()
@@ -34,7 +52,32 @@ def linkify_issues(message: str, base_url: str) -> str:
         )
         return keyword.replace(match.group(2), issue_links)
 
-    return _issue_pattern.sub(_replace_issue_with_link, message)
+    return _ISSUE_PATTERN.sub(_replace_issue_with_link, message)
+
+
+def linkify_external_issues(text):
+    """
+    Detect and convert external GitHub issue references to markdown link addresses.
+
+    Args:
+        text (str): The input text containing GitHub issue references.
+
+    Returns:
+        str: The text with GitHub issue references converted to markdown links.
+
+    Example:
+        >>> linkify_external_issues("See user/repo/#123 for more info.")
+        'See [user/repo/#123](https://github.com/user/repo/issues/123) for more info.'
+    """
+
+    def _replace_match(match):
+        owner = match.group("owner")
+        repo = match.group("repo")
+        issue_number = match.group("issue_number")
+        url = f"https://github.com/{owner}/{repo}/issues/{issue_number}"
+        return f"[{match.group(0)}]({url})"
+
+    return _ISSUE_PATTERN_EXTERNAL.sub(_replace_match, text)
 
 
 class ChangeupCz(ConventionalCommitsCz):
@@ -51,6 +94,7 @@ class ChangeupCz(ConventionalCommitsCz):
     body_indent: int = conf.settings.get("changeup_body_indent", 2)
 
     link_issues: bool = conf.settings.get("changeup_link_issues", True)
+    link_ext_issues: bool = conf.settings.get("changeup_link_external_issues", True)
 
     scope_prefix: str = conf.settings.get("changeup_scope_prefix", "")
     scope_suffix: bool = conf.settings.get("changeup_scope_suffix", "")
@@ -95,6 +139,9 @@ class ChangeupCz(ConventionalCommitsCz):
 
         if self.repo_base_url and self.link_issues:
             msg = linkify_issues(msg, self.repo_base_url)
+
+        if self.link_ext_issues:
+            msg = linkify_external_issues(msg)
 
         parsed_message["message"] = msg
         return parsed_message
